c28f022b31cc06af4e975526de1e65be7d982796
Swapnil Kulkarni <swapnilkulkarni@Swapnils-MacBook-Air.local>
2024-07-04 13:22:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('swapnil.com')
+  })
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
